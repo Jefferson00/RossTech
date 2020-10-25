27e7a4d82ce9c34b6886259ab55a8970ad15abf3
Jefferson00 <jeffersonts00@gmail.com>
2020-10-25 20:42:01 -0300

    update

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+const itens = document.getElementsByClassName("produtos-card-item")
+
+prev.addEventListener("click", function(){
+    console.log(itens)
+})
